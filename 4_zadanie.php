a10fe67d89e66d7965d24d53c6ee9fb5ea8f90cb
Sergeev Oleg <o.v.sergeyev@gmail.com>
2020-07-14 07:39:33 +0300

    Домашнее задание N1

---
+++ 4_zadanie.php
@@ -0,0 +1,16 @@
+<?php
+    $title = "Домашнее задание к первому уроку";
+    $h1 = "Задание N4";
+?>
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title><?=$title?></title>
+</head>
+<body>
+    <h1><?=$h1?></h1>
+
+</body>
+</html>
